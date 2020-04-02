cb342aa01ba14eb494c8ae1bb59b2e1420f50e40
sparksoft-james <52090276+sparksoft-james@users.noreply.github.com>
2020-04-02 21:48:52 +0800

    update

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ if(process.env.NODE_ENV === 'production'){
     app.get('*',(req,res) => res.sendFile(path.resolve(__dirname,'client','build','index.html')));
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`server running on ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
